9f6b2c42cb57b08dd961d53e48c89b3b58152448
K. Yamamoto <kymmt90@gmail.com>
2015-03-04 20:44:54 +0900

    Use Files.AllLines and Stream API in Vocabularies' constructor

--- src/main/java/lda/Vocabularies.java
+++ src/main/java/lda/Vocabularies.java
@@ -1,35 +1,30 @@
 package lda;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Vocabularies {
-    private Map<Integer, Vocabulary> vocabs;
+    private List<Vocabulary> vocabs;
     
     public Vocabularies(String filePath) {
         if (filePath == null) throw new NullPointerException();
 
-        vocabs = new HashMap<>();
-        Path path = Paths.get(filePath);
-        try (BufferedReader reader = Files.newBufferedReader(path)) {
-            int id = 1;
-            String s = null;
-            while ((s = reader.readLine()) != null) {
-                vocabs.put(id, new Vocabulary(id, s));
-                id += 1;
-            }
+        try {
+            Path vocabFilePath = Paths.get(filePath);
+            List<String> lines = Files.readAllLines(vocabFilePath);
+            vocabs = lines.stream().map(v -> new Vocabulary(lines.indexOf(v) + 1, v))
+                                   .collect(Collectors.toList());
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
     }
     
     public Vocabulary get(int id) {
-        return vocabs.get(id);
+        return vocabs.get(id - 1);
     }
     
     public int size() {
