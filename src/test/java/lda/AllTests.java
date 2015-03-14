0343fdc5454a914b6fbe006bf4e39a5a9f66efba
K. Yamamoto <kymmt90@gmail.com>
2015-03-14 12:47:43 +0900

    Add tests for Document

--- src/test/java/lda/AllTests.java
+++ src/test/java/lda/AllTests.java
@@ -23,6 +23,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({AssignmentCounterTest.class,
                BagOfWordsTest.class,
+               DocumentTest.class,
                LDATest.class,
                LDACollapsedGibbsSamplerTest.class,
                LDAInferencePropertiesTest.class,
